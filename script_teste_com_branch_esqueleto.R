8df7173122b5e6de4aaaff32c1a4c2dac4cb6956
nubia-karla-almeida <nubia_karla@id.uff.br>
2026-02-17 21:01:02 -0300

    Adicionando análise descritiva  no script esqueleto

--- script_teste_com_branch_esqueleto.R
+++ script_teste_com_branch_esqueleto.R
@@ -4,7 +4,11 @@
 
 
 # Estatísticas descritivas
-
+min_idade=min(dados$idade)
+max_idade=max(dados$idade)
+media_idade=mean(dados$idade)
+desv_pad=sd(dados$idade)
+dist_freq_sexo=table(dados$sexo)
 
 # Fazer um barplot
-
+barplot(table(dados$sexo),main = "Distribuição das pessoas por sexo", xlab = "Sexo", ylab = "Frequência")
